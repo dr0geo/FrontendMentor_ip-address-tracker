0f6eb41f5aa2aa4de90483c12c4ee78578ce6555
Geoffroy Vie <geoffroy.vie@gmail.com>
2020-10-22 18:36:26 +0100

    Add restricted token

--- resources/js/main.js
+++ resources/js/main.js
@@ -16,7 +16,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZHIwZ2VvIiwiYSI6ImNrZ2t5aTBsYzBiemsyeXBjcmtnZTdraTQifQ.qPcewc-4oa5i2PcuVYBk1g'
+    accessToken: 'pk.eyJ1IjoiZHIwZ2VvIiwiYSI6ImNrZ2wzdW5hdTBjMzkydG1zOWZ3c2hsN2oifQ.v4I6_MpfStGKPaUWebwWaA'
 }).addTo(mymap);
 
 // Create GET request to retrieve information:
