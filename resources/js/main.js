e23f7fce7da86e597617a0377b5f28f06c780f2e
Geoffroy Vie <geoffroy.vie@gmail.com>
2020-10-27 23:21:08 +0000

    Replace XHR with fetch

--- resources/js/main.js
+++ resources/js/main.js
@@ -19,37 +19,35 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoiZHIwZ2VvIiwiYSI6ImNrZ3NpdzlrdTBvMDcyeW1mM3JhdWg0YTYifQ.fxz89-sBEjM2DNbzS2wZCg'
 }).addTo(myMap);
 
-// Create GET request to retrieve information:
+// Fetch information:
 
-const getIpResults = () => {
+const getIpResults = async () => {
   inputValue = document.getElementsByTagName('input')[0].value;
-  return setTimeout(() => {
-    const xhr = new XMLHttpRequest();
-
-    if (inputValue === '') {
-      xhr.open('GET', 'https://cors-anywhere.herokuapp.com/https://geo.ipify.org/api/v1?apiKey=at_twq88aaXycBh9fu7OKwKSXqt3Cq6R');
-    } else {
-      xhr.open('GET', `https://cors-anywhere.herokuapp.com/https://geo.ipify.org/api/v1?apiKey=at_twq88aaXycBh9fu7OKwKSXqt3Cq6R&ipAddress=${inputValue}`);
-    }
+  let url;
     
-    xhr.onreadystatechange = function() {
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        // Relative to written results:
-        const response = JSON.parse(this.responseText);
-        ipAddress.innerHTML = response.ip;
-        locationResult.innerHTML = `${response.location.city}, ${response.location.region}, ${response.location.postalCode}`;
-        timezone.innerHTML = `UTC ${response.location.timezone}`;
-        isp.innerHTML = response.isp;
-        // Relative to map:
-        const coordinates = [response.location.lat, response.location.lng];
-        myMap.flyTo(coordinates);
-        L.marker(coordinates).addTo(myMap);
-      }
-    }
+  if (inputValue === '') {
+    url = 'https://cors-anywhere.herokuapp.com/https://geo.ipify.org/api/v1?apiKey=at_twq88aaXycBh9fu7OKwKSXqt3Cq6R';
 
-  xhr.send();
-  }, 0);
+  } else {
+    url = `https://cors-anywhere.herokuapp.com/https://geo.ipify.org/api/v1?apiKey=at_twq88aaXycBh9fu7OKwKSXqt3Cq6R&ipAddress=${inputValue}`;
+  }
+
+  fetch(url)
+    .then(firstResponse => firstResponse.json())
+    .then(response => {
+      // Relative to written results:
+      ipAddress.innerHTML = response.ip;
+      locationResult.innerHTML = `${response.location.city}, ${response.location.region}, ${response.location.postalCode}`;
+      timezone.innerHTML = `UTC ${response.location.timezone}`;
+      isp.innerHTML = response.isp;
+      // Relative to map:
+      const coordinates = [response.location.lat, response.location.lng];
+      myMap.flyTo(coordinates);
+      L.marker(coordinates).addTo(myMap);
+    });
 }
+
+// Event listeners and calls:
 
 button.addEventListener('click', getIpResults);
 window.addEventListener('keypress', (event) => {
