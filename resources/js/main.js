4c01540a30a3c21fab71cd3b2b53745ee6adc897
Geoffroy Vie <geoffroy.vie@gmail.com>
2020-10-27 22:23:59 +0000

    Change max zoom level

--- resources/js/main.js
+++ resources/js/main.js
@@ -12,7 +12,7 @@ const button = document.getElementsByTagName('button')[0];
 const myMap = L.map('map').setView([53, -5], 9);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 13,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
