6ee04699b81c344ec619534cc56fdad7ad28d4b8
Geoffroy Vie <geoffroy.vie@gmail.com>
2020-10-28 10:58:58 +0000

    Remove zoom control button

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,7 +8,7 @@ const button = document.getElementsByTagName('button')[0];
 
 // Create map:
 
-const myMap = L.map('map').setView([53, -5], 9);
+const myMap = L.map('map', {zoomControl: false}).setView([53, -5], 9);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 13,
